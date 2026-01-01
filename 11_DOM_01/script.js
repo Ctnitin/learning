1922686e0623708c5ecdb1a3e84a1d3924fed783
Ctnitin <nitinjasmai97@gmail.com>
2026-01-01 21:17:51 +0530

    Your message

---
+++ 11_DOM_01/script.js
@@ -0,0 +1,20 @@
+// alert('hey there,i am a alert');
+//for input
+// const value = prompt("whats your name?",'nitin');
+// console.log(value);/
+
+// window --> browser ko window kehte hai
+// window.console.log('Nitin Chauhan');
+// window.alert("Hey Nitin");
+
+// document --> your own code will available in Document
+// document.console.log("Hello Nitin");
+// console.log(document.title);
+// document.write("THIS is HACKED");
+
+const name = prompt("Write your name ?", 'Nitin Chauhan' )
+document.write("Hello " + name);
+
+// with the help of document.write you can write in your page;
+// with the help of window.alert you can show alert box
+// with the help of prompt you can take input from user
